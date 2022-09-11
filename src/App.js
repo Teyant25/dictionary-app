1f41f6b927558880a1d13a530b82ed5b62009070
Teyant25 <96316475+Teyant25@users.noreply.github.com>
2022-09-11 15:31:26 -0400

    deleted logo info. Added Dictionary component and css info

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+          <footer className="text-center">
+            <small> Coded by Tya Kent</small>{" "}
+          </footer>
+        </main>
+      </div>
     </div>
   );
 }
